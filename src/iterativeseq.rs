4d13a6b303cb9a17b9fa250902fe95028d98d5c2
Amr Hassan <amr.hassan@gmail.com>
2017-08-25 13:18:30 +0200

    iterativeseq: Iterator for IterativeSeq

--- src/iterativeseq.rs
+++ src/iterativeseq.rs
@@ -18,12 +18,24 @@ impl IterativeSeq {
 
     /// Length of the sequence from start till convergence (WARNING: Does not terminate if seq is non-converging)
     pub fn length(&self, convergence: u64) -> usize {
-        let mut i = self.first;
-        let mut length = 1;
-        while i != convergence {
-            length += 1;
-            i = (self.f)(i);
-        }
-        length
+        self.iter().take_while(|&n| n != convergence).count()
+    }
+
+    pub fn iter(&self) -> IterativeSeqIterator {
+        IterativeSeqIterator { seq: self, last: self.first }
+    }
+}
+
+struct IterativeSeqIterator<'a> {
+    seq: &'a IterativeSeq,
+    last: u64,
+}
+
+impl <'a> Iterator for IterativeSeqIterator<'a> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<u64> {
+        self.last = (self.seq.f)(self.last);
+        Some(self.last)
     }
 }
